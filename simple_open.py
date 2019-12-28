ab9ff641505a30c56274368ab118342ae95abc11
Tal Zoor <zoor@post.bgu.ac.il>
2019-12-28 18:33:52 +0200

    2 dec place for time open

--- simple_open.py
+++ simple_open.py
@@ -145,8 +145,8 @@ def check_time_to_run_pot(_time_to_run_pot):
 
 
 def open_valve(_pinA, _pinB, _open_time):
-    print("Opening {} and {} for {} {}".format(_pinA, _pinB,
-                                               _open_time if _open_time >= 1 else  _open_time*60 ,
+    print("Opening {} and {} for {0:.2f} {}".format(_pinA, _pinB,
+                                               _open_time if _open_time >= 1 else _open_time*60,
                                                "min" if _open_time >= 1 else "sec"))
     GPIO.output(_pinA, GPIO.HIGH)
     GPIO.output(_pinB, GPIO.HIGH)
