e28bf65a69ab1f29ffb0dcfca266c78a29e6400b
talzoor <talzoor@gmail.com>
2020-05-19 13:07:18 +0300

    settings times update

--- Settings.ino
+++ Settings.ino
@@ -1,9 +1,9 @@
 [Program 1]
-run time (flowerbed) = 5       # min
-run time (flowerpot) = 3       # min
+run time (flowerbed) = 8       # min
+run time (flowerpot) = 4       # min
 
 [Program 2]
-run time (flowerbed) = 1       # min
+run time (flowerbed) = 4       # min
 run time (flowerpot) = 2       # min
 
 
