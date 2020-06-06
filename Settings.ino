7e9f47f4b265e9beb7c77ca3feccd5651e4ef7f4
talzoor <talzoor@gmail.com>
2020-06-07 01:32:33 +0300

    settings times update and lib fix

--- Settings.ino
+++ Settings.ino
@@ -1,10 +1,10 @@
 [Program 1]
-run time (flowerbed) = 10       # min
-run time (flowerpot) = 2.5       # min
+run time (flowerbed) = 11       # min
+run time (flowerpot) = 3       # min
 
 [Program 2]
-run time (flowerbed) = 3       # min
-run time (flowerpot) = 1.5       # min
+run time (flowerbed) = 4       # min
+run time (flowerpot) = 2       # min
 
 
 
